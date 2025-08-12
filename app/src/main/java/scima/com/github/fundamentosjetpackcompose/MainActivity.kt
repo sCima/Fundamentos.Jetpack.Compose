36a60d5494109ffc6bf9d4464677da1b526c88a2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-11 22:22:48 -0300

    Final da aula 08/11 - Commit BOX

--- app/src/main/java/scima/com/github/fundamentosjetpackcompose/MainActivity.kt
+++ app/src/main/java/scima/com/github/fundamentosjetpackcompose/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import scima.com.github.fundamentosjetpackcompose.ui.theme.FundamentosJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +30,64 @@ class MainActivity : ComponentActivity() {
         setContent {
             FundamentosJetpackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    NotificationBage(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Inicial(modifier: Modifier = Modifier) {
+    TextoCustomizado("Primeira função composable.")
+    Column(modifier = modifier.background(color = Color.Blue)) {
+        TextoCustomizado("Primeira função composable.", modifier)
+        Row {
+            TextoCustomizado("Seja bem vindo!")
+            TextoCustomizado("Olá usuário.")
+    }
+    }
+}
+@Composable
+fun TextoCustomizado(texto: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = texto,
+        color = Color.White,
         modifier = modifier
     )
 }
 
+@Composable
+fun NotificationBage(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.padding(16.dp), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .size(24.dp)
+                .clip(CircleShape)
+                .background(color = Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(8.dp)
+                .clip(CircleShape)
+                .background(color = Color.Red)
+                .align(Alignment.TopEnd)
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewFuncaoInicial() {
     FundamentosJetpackComposeTheme {
-        Greeting("Android")
+        Inicial()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewNotificationBage() {
+    FundamentosJetpackComposeTheme {
+        NotificationBage()
     }
 }
